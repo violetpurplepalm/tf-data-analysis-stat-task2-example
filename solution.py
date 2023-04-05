8fcd5bd853f2139f91b1a5a1837521d19501e2b4
violetpurplepalm <45032239+violetpurplepalm@users.noreply.github.com>
2023-04-05 15:09:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,5 +10,5 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     loc = x.mean()
     #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return  loc *np.sqrt(chi2.ppf(1 - alpha / 2, 1)/(27*2)), \
-           loc *np.sqrt(chi2.ppf(alpha / 2, 1)/(27*2))
+    return  loc *np.sqrt(chi2.ppf(alpha / 2, 1)/(27*2)), \
+           loc *np.sqrt(chi2.ppf(1 - alpha / 2, 1)/(27*2))
